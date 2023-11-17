68fe8424a567e70d941803313653efa932c346ad
Apple Iphone <abcd@Apples-MacBook-Pro-2.local>
2023-11-17 14:58:33 -0500

    post request works, it is returning xml , use different api to get json response

---
+++ api/controllers/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// Load each controller
+const microPostsController = require("./microPosts.js");
+const ebayApiRouter = require('./ebay-api');
+
+// Mount each controller under a specific route. These
+// will be prefixes to all routes defined inside the controller
+router.use("/micro_posts", microPostsController);
+router.use('/ebay-api', ebayApiRouter);
+
+
+module.exports = router;

f156ed9646edd9e4970794a9c6fb6ee3cd929534
popki222 <135931155+popki222@users.noreply.github.com>
2023-12-01 15:18:45 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import PostsListPage from "./pages/PostsListPage";
+import PostFormPage from "./pages/PostFormPage";
+import ShowPostPage from "./pages/ShowPostPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import Form from "./components/Form"
+
+import "./App.css";
+
+function Navigation(props) {
+  async function handleClick() {
+    try {
+      const response = await fetch('http://localhost:8080/api/ebay-api', {method: "post"});
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Response from backend:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Micro Blog
+        </Link>
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/posts/new">
+              Create a Micro Post
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about-us">
+              About Us
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <button className="btn btn-primary" onClick={handleClick}>
+              Trigger eBay API
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Form />
+      <div className="container-xl text-center">
+        <div className="row justify-content-center">
+          <Routes>
+            <Route path="/posts/new" element={<PostFormPage />} />
+            <Route path="/posts/:id" element={<ShowPostPage />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/" element={<PostsListPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
